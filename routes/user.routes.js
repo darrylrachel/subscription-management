8331e526d749b363e50352c59a44444910ae91bd
Darryl Rachel <racheldarryl@icloud.com>
2025-02-22 14:56:28 -0800

    Setup environments, and routes

---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+
+const userRouter = Router();
+
+userRouter.get('/', (req, res) => { res.json( { title: 'GET all users'} ) })
+
+userRouter.get('/:id', (req, res) => { res.json( { title: 'GET user details'} ) })
+
+userRouter.post('/', (req, res) => { res.json( { title: 'CREATE new user'} ) })
+
+userRouter.put('/:id', (req, res) => { res.json( { title: 'UPDATE user'} ) })
+
+userRouter.delete('/:id', (req, res) => { res.json( { title: 'DELETE user'} ) })
+
+export default userRouter;
