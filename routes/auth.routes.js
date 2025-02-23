66a33c8ae83c5e284e459d71a4b186d0c0c01258
Darryl Rachel <racheldarryl@icloud.com>
2025-02-22 16:58:52 -0800

    configured controllers, database, middleware, auth routes. Need to fix UserActivation not defined error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
+import { signUp, signIn, signOut } from '../contollers/auth.controller.js';
+
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => { res.json({ title: 'Sign Up'})})
-
-authRouter.post('/sign-in', (req, res) => { res.json({ title: 'Sign In'})})
-
-authRouter.post('/sign-out', (req, res) => { res.json({ title: 'Sign Out'})})
+// Path: /api/v1/auth/sign-up (POST)
+authRouter.post('/sign-up', signUp)
+authRouter.post('/sign-in', signIn)
+authRouter.post('/sign-out', signOut)
 
 export default authRouter;
